f8cd039eaf054f292a8f980bb7b4238cfd6e54b7
AmirhosseinVeysi <amirhossein_veysi@yahoo.com>
2021-12-30 14:32:32 +0330

    first commit

---
+++ db/db.lua
@@ -0,0 +1,9 @@
+local db
+
+addEventHandler("onResourceStart", resourceRoot, function() 
+    db = dbConnect('mysql', 'dbname=mtaserver;host=localhost', 'root', '')
+end)
+
+function getConnection() 
+    return db
+end
